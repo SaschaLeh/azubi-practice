86ac9a9f365666b1bae532d88e82b3d963fa613b
Sascha Lehmann <sascha.lehmann92@gmail.com>
2021-12-16 14:19:06 +0100

    Lösung Aufgabe 2

--- benutzerverwaltung/src/app/app.component.ts
+++ benutzerverwaltung/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppUser } from './model/app-user.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  public appUsers: AppUser[] = [
+    {
+      salutation: 'Herr',
+      name: 'Hans',
+      surename: 'Zimmer',
+      email: 'hans.zimmer@xzy.de',
+      age: 21,
+    },
+    {
+      salutation: 'Frau',
+      name: 'Margarete',
+      surename: 'Zimmer',
+      email: 'margarete.zimmer@xzy.de',
+      age: 60,
+    },
+    {
+      salutation: 'Frau',
+      name: 'Maximiliane',
+      surename: 'Musterfrau',
+      email: 'maximiliane.musterfrau@xzy.de',
+      age: 40,
+    },
+  ];
   title = 'Benutzerverwaltung';
 }
---
+++ benutzerverwaltung/src/app/model/app-user.model.ts
@@ -0,0 +1,7 @@
+export interface AppUser {
+  salutation: string;
+  name: string;
+  surename: string;
+  email: string;
+  age: number;
+}
